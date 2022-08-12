132dc2afb9e6c6e52bc8da4adaca7ffab0cbb7ca
Muthumali <muthudil1999@gmail.com>
2022-08-12 23:03:33 +0530

    add Ui

---
+++ src/controller/clientController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+
+public class clientController {
+    public TextArea txtClientPane;
+    public JFXTextField txtClientMessage;
+
+    public void sendOnAction(ActionEvent actionEvent) {
+    }
+}
